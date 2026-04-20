eac4d89d85ac52fcf759b6ab76ae6e470ceff517
いたこす <109420050+itakosu55@users.noreply.github.com>
2026-04-21 03:34:47 +0900

    feat: Init linter (ESLint, Prettier)

---
+++ eslint.config.mjs
@@ -0,0 +1,15 @@
+// @ts-check
+
+import js from '@eslint/js';
+import { defineConfig } from 'eslint/config';
+import tseslint from 'typescript-eslint';
+import eslintConfigPrettier from 'eslint-config-prettier/flat';
+
+export default defineConfig(
+  {
+    ignores: ['dist/'],
+  },
+  js.configs.recommended,
+  tseslint.configs.recommended,
+  eslintConfigPrettier
+);
